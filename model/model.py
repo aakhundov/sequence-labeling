b5e85595e161ac456ac11f812fb08d697f9c22b4
Adnan Akhundov <adnan.akhundov@gmail.com>
2018-02-11 11:28:11 +0100

    Minor code fixes in model_fn.

--- model/model.py
+++ model/model.py
@@ -84,7 +84,7 @@ def model_fn(input_values, embedding_words, embedding_matrix, label_vocab,
     tf_max_sentence_len = tf.shape(tf_word_embeddings)[1]
 
     tf_char_lstm_fw, tf_char_lstm_bw = create_layered_bi_lstm(
-        char_lstm_layers, char_lstm_units, 0
+        char_lstm_layers, char_lstm_units, tf_dropout_rate
     )
 
     ((tf_char_outputs_fw, tf_char_outputs_bw), (_, _)) = tf.nn.bidirectional_dynamic_rnn(
@@ -93,7 +93,7 @@ def model_fn(input_values, embedding_words, embedding_matrix, label_vocab,
         dtype=tf.float32, scope="char_rnn"
     )
 
-    tf_last_indices, tf_range = tf.maximum(0, tf_char_seq_len-1)
+    tf_last_indices = tf.maximum(0, tf_char_seq_len-1)
     tf_gather_indices = tf.transpose(tf.stack((tf.range(tf.shape(tf_char_seq_len)[0]), tf_last_indices)))
     tf_char_last_outputs_fw = tf.gather_nd(tf_char_outputs_fw, tf_gather_indices)
     tf_char_last_outputs_bw = tf.gather_nd(tf_char_outputs_bw, tf_gather_indices)
