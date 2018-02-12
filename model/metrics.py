79face7d21121120fdd8aa3766dc3512249a98e8
Adnan Akhundov <adnan.akhundov@gmail.com>
2018-02-12 23:26:50 +0100

    Metrics have been reworked to adapt to IOB/F1/accuracy performance reporting.

--- model/metrics.py
+++ model/metrics.py
@@ -33,8 +33,23 @@ def compute_f1_score(tp, fp, fn):
     return prec * 100, rec * 100, f1 * 100
 
 
-def compute_iob_metrics(gold, predicted, seq_len, num_labels):
-    num_classes = num_labels // 2
+def are_iob_labels(label_names):
+    for lb in label_names:
+        if not (lb.startswith("I-") or lb.startswith("B-") or lb == "O"):
+            return False
+    return True
+
+
+def f1_scores_required(labels, num_labels, seq_len, min_zero_ratio=0.8):
+    num_zero = sum([sum([
+        1 for lb in labels[i][:seq_len[i]]
+        if lb == num_labels - 1
+    ]) for i in range(len(seq_len))])
+    return num_zero / sum(seq_len) >= min_zero_ratio
+
+
+def compute_metrics(gold, predicted, seq_len, label_names):
+    num_labels = len(label_names)
 
     conf = np.zeros([num_labels, num_labels], dtype=np.int32)
     for i in range(len(gold)):
@@ -45,51 +60,65 @@ def compute_iob_metrics(gold, predicted, seq_len, num_labels):
 
     acc = np.sum(np.diag(conf)) / sum(seq_len) * 100
 
-    b_tp = np.sum(conf[:num_classes, :num_classes])
-    b_tn = np.sum(conf[num_classes:, num_classes:])
-    b_fp = np.sum(conf[num_classes:, :num_classes])
-    b_fn = np.sum(conf[:num_classes, num_classes:])
-    b_prec, b_rec, b_f1 = compute_f1_score(b_tp, b_fp, b_fn)
+    iob_labels = are_iob_labels(label_names)
+    f1_required = f1_scores_required(gold, num_labels, seq_len)
 
-    b_acc = np.sum(np.diag(conf[:num_classes])) / np.sum(conf[:num_classes]) * 100
-    e_acc = np.sum(np.diag(conf[:num_labels-1])) / np.sum(conf[:num_labels-1]) * 100
-    o_acc = conf[-1, -1] / np.sum(conf[-1]) * 100
-
-    gold_entities, gold_spans = extract_entities(gold, seq_len, num_labels, num_classes)
-    predicted_entities, predicted_spans = extract_entities(predicted, seq_len, num_labels, num_classes)
-
-    e_tp = len([1 for p in predicted_spans if p in gold_spans])
-    e_fp = len([1 for p in predicted_spans if p not in gold_spans])
-    e_fn = len([1 for p in gold_spans if p not in predicted_spans])
-    e_prec, e_rec, e_f1 = compute_f1_score(e_tp, e_fp, e_fn)
-
-    ec_tp = len([1 for p in predicted_entities if p in gold_entities])
-    ec_fp = len([1 for p in predicted_entities if p not in gold_entities])
-    ec_fn = len([1 for p in gold_entities if p not in predicted_entities])
-    ec_prec, ec_rec, ec_f1 = compute_f1_score(ec_tp, ec_fp, ec_fn)
-
-    result = {
-        "acc": acc, "B_acc": b_acc, "E_acc": e_acc, "O_acc": o_acc,
-        "B_TP": b_tp, "B_TN": b_tn, "B_FP": b_fp, "B_FN": b_fn,
-        "B_prec": b_prec, "B_rec": b_rec, "B_F1": b_f1,
-        "E_TP": e_tp, "E_FP": e_fp, "E_FN": e_fn,
-        "E_prec": e_prec, "E_rec": e_rec, "E_F1": e_f1,
-        "EC_TP": ec_tp, "EC_FP": ec_fp, "EC_FN": ec_fn,
-        "EC_prec": ec_prec, "EC_rec": ec_rec, "EC_F1": ec_f1,
-        "confusion": conf
+    results = {
+        "acc": acc, "confusion": conf,
+        "F1": iob_labels or f1_required,
+        "IOB": iob_labels
     }
 
-    for i in range(num_classes):
-        ec_class_tp = len([1 for p in predicted_entities if p[2] == i and p in gold_entities])
-        ec_class_fp = len([1 for p in predicted_entities if p[2] == i and p not in gold_entities])
-        ec_class_fn = len([1 for p in gold_entities if p[2] == i and p not in predicted_entities])
-        ec_class_prec, ec_class_rec, ec_class_f1 = compute_f1_score(ec_class_tp, ec_class_fp, ec_class_fn)
+    if results["F1"]:
+        num_classes = num_labels // 2 if iob_labels else num_labels - 1
 
-        result["EC_" + str(i) + "_TP"] = ec_class_tp
-        result["EC_" + str(i) + "_FP"] = ec_class_fp
-        result["EC_" + str(i) + "_FN"] = ec_class_fn
-        result["EC_" + str(i) + "_prec"] = ec_class_prec
-        result["EC_" + str(i) + "_rec"] = ec_class_rec
-        result["EC_" + str(i) + "_F1"] = ec_class_f1
+        b_tp = np.sum(conf[:num_classes, :num_classes])
+        b_tn = np.sum(conf[num_classes:, num_classes:])
+        b_fp = np.sum(conf[num_classes:, :num_classes])
+        b_fn = np.sum(conf[:num_classes, num_classes:])
+        b_prec, b_rec, b_f1 = compute_f1_score(b_tp, b_fp, b_fn)
 
-    return result
+        b_acc = np.sum(np.diag(conf[:num_classes])) / np.sum(conf[:num_classes]) * 100
+        e_acc = np.sum(np.diag(conf[:num_labels-1])) / np.sum(conf[:num_labels-1]) * 100
+        o_acc = conf[-1, -1] / np.sum(conf[-1]) * 100
+
+        gold_entities, gold_spans = extract_entities(gold, seq_len, num_labels, num_classes)
+        predicted_entities, predicted_spans = extract_entities(predicted, seq_len, num_labels, num_classes)
+
+        e_tp = len([1 for p in predicted_spans if p in gold_spans])
+        e_fp = len([1 for p in predicted_spans if p not in gold_spans])
+        e_fn = len([1 for p in gold_spans if p not in predicted_spans])
+        e_prec, e_rec, e_f1 = compute_f1_score(e_tp, e_fp, e_fn)
+
+        ec_tp = len([1 for p in predicted_entities if p in gold_entities])
+        ec_fp = len([1 for p in predicted_entities if p not in gold_entities])
+        ec_fn = len([1 for p in gold_entities if p not in predicted_entities])
+        ec_prec, ec_rec, ec_f1 = compute_f1_score(ec_tp, ec_fp, ec_fn)
+
+        f1_metrics = {
+            "B_acc": b_acc, "E_acc": e_acc, "O_acc": o_acc,
+            "B_TP": b_tp, "B_TN": b_tn, "B_FP": b_fp, "B_FN": b_fn,
+            "B_prec": b_prec, "B_rec": b_rec, "B_F1": b_f1,
+            "E_TP": e_tp, "E_FP": e_fp, "E_FN": e_fn,
+            "E_prec": e_prec, "E_rec": e_rec, "E_F1": e_f1,
+            "EC_TP": ec_tp, "EC_FP": ec_fp, "EC_FN": ec_fn,
+            "EC_prec": ec_prec, "EC_rec": ec_rec, "EC_F1": ec_f1,
+        }
+
+        for i in range(num_classes):
+            ec_class_tp = len([1 for p in predicted_entities if p[2] == i and p in gold_entities])
+            ec_class_fp = len([1 for p in predicted_entities if p[2] == i and p not in gold_entities])
+            ec_class_fn = len([1 for p in gold_entities if p[2] == i and p not in predicted_entities])
+            ec_class_prec, ec_class_rec, ec_class_f1 = compute_f1_score(ec_class_tp, ec_class_fp, ec_class_fn)
+
+            f1_metrics["EC_" + str(i) + "_TP"] = ec_class_tp
+            f1_metrics["EC_" + str(i) + "_FP"] = ec_class_fp
+            f1_metrics["EC_" + str(i) + "_FN"] = ec_class_fn
+            f1_metrics["EC_" + str(i) + "_prec"] = ec_class_prec
+            f1_metrics["EC_" + str(i) + "_rec"] = ec_class_rec
+            f1_metrics["EC_" + str(i) + "_F1"] = ec_class_f1
+
+        for m in f1_metrics:
+            results[m] = f1_metrics[m]
+
+    return results
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 from model.input import input_fn
 from model.model import model_fn
-from model.metrics import compute_iob_metrics
+from model.metrics import compute_metrics
 
 
 EPOCHS = 100
@@ -16,66 +16,89 @@ POLYGLOT_FILE = "polyglot/polyglot-en.pkl"
 REPORT_IOB_SCORES = True
 
 
-print("Setting up input pipeline...")
-
-with tf.device("/cpu:0"):
-    train_data = input_fn(
-        tf.data.TextLineDataset(TASK_DATA_FOLDER + "train.txt"),
-        batch_size=BATCH_SIZE, shuffle_data=True
-    )
-    val_data = input_fn(tf.data.TextLineDataset(TASK_DATA_FOLDER + "val.txt"), shuffle_data=False)
-    test_data = input_fn(tf.data.TextLineDataset(TASK_DATA_FOLDER + "test.txt"), shuffle_data=False)
-
-data_handle = tf.placeholder(tf.string, shape=())
-next_input_values = tf.data.Iterator.from_string_handle(
-    data_handle, train_data.output_types, train_data.output_shapes
-).get_next()
-
-print("Loading embedding data...")
-
-label_names = [line[:-1] for line in open(TASK_DATA_FOLDER + "labels.txt").readlines()]
-polyglot_words, polyglot_embeddings = pickle.load(open(POLYGLOT_FILE, "rb"), encoding="bytes")
-
-print("Building the model...")
-
-train_op, loss, accuracy, predictions, labels, sentence_length, sentences, dropout_rate = model_fn(
-    next_input_values, polyglot_words, polyglot_embeddings, label_names
-)
-
-
-with tf.Session() as sess:
-    print("Initializing variables...")
-
-    sess.run(tf.tables_initializer())
-    sess.run(tf.global_variables_initializer())
-
-    train_handle = sess.run(train_data.make_one_shot_iterator().string_handle())
-    val_handle = sess.run(val_data.make_one_shot_iterator().string_handle())
-    test_handle = sess.run(test_data.make_one_shot_iterator().string_handle())
-
-    print("Training...")
-    print()
-
-    for epoch in range(EPOCHS):
-        for step in range(STEPS_PER_EPOCH):
-            sess.run(train_op, feed_dict={data_handle: train_handle, dropout_rate: 0.5})
-
-        val_acc, val_loss, val_labels, val_predictions, val_sentence_len = sess.run(
-            [accuracy, loss, labels, predictions, sentence_length],
-            feed_dict={data_handle: val_handle}
-        )
-
-        if REPORT_IOB_SCORES:
-            iob_metrics = compute_iob_metrics(
-                val_labels, val_predictions, val_sentence_len, len(label_names)
-            )
-
-            print("{:<16} {:<39} {:<30} {:<30} {}".format(
-                "{0}.val: L {1:.3f}".format(epoch + 1, val_loss),
-                "acc [B {d[B_acc]:.2f} E {d[E_acc]:.2f} O {d[O_acc]:.2f} T {d[acc]:.2f}]".format(d=iob_metrics),
-                "B [P {d[B_prec]:.2f} R {d[B_rec]:.2f} F1 {d[B_F1]:.2f}]".format(d=iob_metrics),
-                "E [P {d[E_prec]:.2f} R {d[E_rec]:.2f} F1 {d[E_F1]:.2f}]".format(d=iob_metrics),
-                "EC [P {d[EC_prec]:.2f} R {d[EC_rec]:.2f} F1 {d[EC_F1]:.2f}]".format(d=iob_metrics),
+def report_performance(epoch, metrics, loss, label_names):
+    if metrics["F1"]:
+        if metrics["IOB"]:
+            print("{:<17} {:<39} {:<30} {:<30} {}".format(
+                "{0}.val: L {1:.3f}".format(epoch, loss),
+                "acc [B {d[B_acc]:.2f} E {d[E_acc]:.2f} O {d[O_acc]:.2f} T {d[acc]:.2f}]".format(d=metrics),
+                "B [P {d[B_prec]:.2f} R {d[B_rec]:.2f} F1 {d[B_F1]:.2f}]".format(d=metrics),
+                "E [P {d[E_prec]:.2f} R {d[E_rec]:.2f} F1 {d[E_F1]:.2f}]".format(d=metrics),
+                "EC [P {d[EC_prec]:.2f} R {d[EC_rec]:.2f} F1 {d[EC_F1]:.2f}]".format(d=metrics)
             ))
         else:
-            print(epoch + 1, "val acc", val_acc)
+            if len(label_names) > 2:
+                print("{:<17} {:<30} {:<30} {}".format(
+                    "{0}.val: L {1:.3f}".format(epoch, loss),
+                    "acc [E {d[E_acc]:.2f} O {d[O_acc]:.2f} T {d[acc]:.2f}]".format(d=metrics),
+                    "E [P {d[E_prec]:.2f} R {d[E_rec]:.2f} F1 {d[E_F1]:.2f}]".format(d=metrics),
+                    "EC [P {d[EC_prec]:.2f} R {d[EC_rec]:.2f} F1 {d[EC_F1]:.2f}]".format(d=metrics)
+                ))
+            else:
+                print("{:<17} {:<30} {}".format(
+                    "{0}.val: L {1:.3f}".format(epoch, loss),
+                    "acc [E {d[E_acc]:.2f} O {d[O_acc]:.2f} T {d[acc]:.2f}]".format(d=metrics),
+                    "E [P {d[E_prec]:.2f} R {d[E_rec]:.2f} F1 {d[E_F1]:.2f}]".format(d=metrics)
+                ))
+    else:
+        print("{:<17} {}".format(
+            "{0}.val: L {1:.3f}".format(epoch, loss),
+            "acc {d[acc]:.2f}".format(d=metrics)
+        ))
+
+
+def train():
+    print("Setting up input pipeline...")
+
+    with tf.device("/cpu:0"):
+        train_data = input_fn(
+            tf.data.TextLineDataset(TASK_DATA_FOLDER + "train.txt"),
+            batch_size=BATCH_SIZE, shuffle_data=True
+        )
+        val_data = input_fn(tf.data.TextLineDataset(TASK_DATA_FOLDER + "val.txt"))
+        # test_data = input_fn(tf.data.TextLineDataset(TASK_DATA_FOLDER + "test.txt"))
+
+    data_handle = tf.placeholder(tf.string, shape=())
+    next_input_values = tf.data.Iterator.from_string_handle(
+        data_handle, train_data.output_types, train_data.output_shapes
+    ).get_next()
+
+    print("Loading embedding data...")
+
+    label_names = [line[:-1] for line in open(TASK_DATA_FOLDER + "labels.txt").readlines()]
+    polyglot_words, polyglot_embeddings = pickle.load(open(POLYGLOT_FILE, "rb"), encoding="bytes")
+
+    print("Building the model...")
+
+    train_op, loss, accuracy, predictions, labels, sentence_length, sentences, dropout_rate = model_fn(
+        next_input_values, polyglot_words, polyglot_embeddings, label_names
+    )
+
+    with tf.Session() as sess:
+        print("Initializing variables...")
+
+        sess.run(tf.tables_initializer())
+        sess.run(tf.global_variables_initializer())
+
+        train_handle = sess.run(train_data.make_one_shot_iterator().string_handle())
+        val_handle = sess.run(val_data.make_one_shot_iterator().string_handle())
+        # test_handle = sess.run(test_data.make_one_shot_iterator().string_handle())
+
+        print("Training...")
+        print()
+
+        for epoch in range(EPOCHS):
+            for step in range(STEPS_PER_EPOCH):
+                sess.run(train_op, feed_dict={data_handle: train_handle, dropout_rate: 0.5})
+
+            val_loss, val_labels, val_predictions, val_sentence_len = sess.run(
+                [loss, labels, predictions, sentence_length],
+                feed_dict={data_handle: val_handle}
+            )
+
+            metrics = compute_metrics(val_labels, val_predictions, val_sentence_len, label_names)
+            report_performance(epoch + 1, metrics, val_loss, label_names)
+
+
+if __name__ == "__main__":
+    train()
